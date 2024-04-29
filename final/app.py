54b5f24fb1fc75cc9dee1597a95f8fc4afbca4b3
CANix2 <112523124+CANix2@users.noreply.github.com>
2024-04-29 12:51:42 -0400

    Update app.py

--- final/app.py
+++ final/app.py
@@ -1,13 +1,6 @@
 from flask import Flask, render_template, jsonify
-from ISStreamer.Streamer import Streamer
 import random
 from grip_strength_tester import read_input
-
-streamer = Streamer(
-    bucket_name="KG",
-    bucket_key="REEQFQN4KYAU",
-    access_key="ist_e5y21RVgoiT_lMfkC1wgrb85Y_DtStmi"
-)
 
 app = Flask(__name__)
 
@@ -18,6 +11,4 @@ def main():
 @app.route('/update_progress')
 def get_progress():
     progress = read_input()
-    streamer.log("KG", progress)
-    streamer.flush()
     return jsonify({'progress': progress})
